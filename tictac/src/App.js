789d308ef20ef00f1a2eeac2c558ae9f60e205a2
Dinesh332003 <dineshvishnoi2003@gmail.com>
2024-03-01 17:19:22 +0530

    asasas

---
+++ tictac/src/App.js
@@ -0,0 +1,17 @@
+import logo from './logo.svg';
+import './App.css';
+import Game from './Test';
+import { Board } from './Test';
+import Explaination from './Explaination';
+
+
+function App() {
+  return (
+    <div className="App">
+  <Board/>
+  {/* <Explaination/> */}
+    </div>
+  );
+}
+
+export default App;
